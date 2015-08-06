1684a7628c13b8bc4901b0e63c179cd9467c7787
Robin McDonald <r.m.donald@gmail.com>
2015-08-06 16:04:54 +1000

    Added Plot.R

---
+++ Plot1.R
@@ -0,0 +1,18 @@
+# Generate Plot1
+# Exploratory Data Analysis
+# Project 1
+#
+#Load Data, unzip and generate Plot1
+#
+if(!file.exists("./data")){
+  dir.create("./data")
+}
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+fileD <- "./data/household_power_consumption.zip"
+if(!file.exists(fileD)){
+  download.file(fileUrl,destfile=fileD,mode="wb")
+}
+
+unzip(fileD, exdir="./data")
+
