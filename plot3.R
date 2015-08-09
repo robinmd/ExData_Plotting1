00f94fce0283a588e0056421954af7b50a9c9fd8
Robin McDonald <r.m.donald@gmail.com>
2015-08-09 18:30:08 +1000

    fixed plot3

--- plot3.R
+++ plot3.R
@@ -33,6 +33,13 @@ plot(x=hpcFeb$DateTime,y=hpcFeb$Sub_metering_1,
         type='l',
         xlab="",
        ylab="Energy sub Metering")
+lines(x=hpcFeb$DateTime,y=hpcFeb$Sub_metering_2, col="red")
+lines(x=hpcFeb$DateTime,y=hpcFeb$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col=c("black","red","blue"),
+       lty=c(1,1),lwd=c(2.5,2.5)
+       )
 
 dev.off()
+
 
